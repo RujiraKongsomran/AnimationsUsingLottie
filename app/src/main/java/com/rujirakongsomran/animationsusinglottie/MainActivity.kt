583dc503d03b5de5bc641dc7bacf933596921315
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-16 00:03:26 +0700

    finish case 5

--- app/src/main/java/com/rujirakongsomran/animationsusinglottie/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/animationsusinglottie/MainActivity.kt
@@ -7,21 +7,34 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var isCheckedDone = false
+//    var isCheckedDone = false
+    var isSwitchOn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lottieCheckedDone.setOnClickListener {
-            if (isCheckedDone) {
-                lottieCheckedDone.speed = -1f
-                lottieCheckedDone.playAnimation()
-                isCheckedDone = false
+//        lottieCheckedDone.setOnClickListener {
+//            if (isCheckedDone) {
+//                lottieCheckedDone.speed = -1f
+//                lottieCheckedDone.playAnimation()
+//                isCheckedDone = false
+//            } else {
+//                lottieCheckedDone.speed = 1f
+//                lottieCheckedDone.playAnimation()
+//                isCheckedDone = true
+//            }
+//        }
+        lottieSwitchButton.setOnClickListener {
+            if (isSwitchOn) {
+                lottieSwitchButton.setMinAndMaxProgress(0.5f, 1.0f)
+                lottieSwitchButton.playAnimation()
+                isSwitchOn = false
             } else {
-                lottieCheckedDone.speed = 1f
-                lottieCheckedDone.playAnimation()
-                isCheckedDone = true
+                lottieSwitchButton.setMinAndMaxProgress(0.0f, 0.5f)
+                lottieSwitchButton.playAnimation()
+                isSwitchOn = true
             }
+
         }
 
     }
