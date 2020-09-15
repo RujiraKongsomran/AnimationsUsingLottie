8349d77cb19bdf41b2c34e4878d94f3ba80cf703
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-09-15 23:50:41 +0700

    finish case 4

--- app/src/main/java/com/rujirakongsomran/animationsusinglottie/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/animationsusinglottie/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rujirakongsomran.animationsusinglottie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isCheckedDone = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lottieCheckedDone.setOnClickListener {
+            if (isCheckedDone) {
+                lottieCheckedDone.speed = -1f
+                lottieCheckedDone.playAnimation()
+                isCheckedDone = false
+            } else {
+                lottieCheckedDone.speed = 1f
+                lottieCheckedDone.playAnimation()
+                isCheckedDone = true
+            }
+        }
+
     }
 }
